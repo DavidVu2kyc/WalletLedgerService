e79da521cfd48e4eaf01288cb5afd341cbaeb6c7
davidvu <quangdepzaivt03@gmail.com>
2026-09-15 23:29:14 +0700

    fix windows conflict

--- wallet/src/main/java/com/example/wallet/WalletApplication.java
+++ wallet/src/main/java/com/example/wallet/WalletApplication.java
@@ -1,5 +1,6 @@
 package com.example.wallet;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WalletApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     SpringApplication.run(WalletApplication.class, args);
   }
 }
